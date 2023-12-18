0fe52745f87a1ee82a98e103cc29bcf98e608316
MarianaNamie <mariana.ribeiro@sptech.school>
2023-12-17 22:05:32 -0300

    ajustes no site institucional

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -5,7 +5,6 @@ import { AiOutlineClose } from "react-icons/ai";
 import scrollToHelper from "../../helpers/scrollHelper";
 
 function NavBar({ onLoginModal }) {
-  
   function handleMenuMobile() {
     const menu = document.getElementById("navbar-mobile");
     menu.classList.toggle("active");
@@ -18,19 +17,46 @@ function NavBar({ onLoginModal }) {
       <div className="navbar-content">
         <h3>MindTech</h3>
         <FiMenu className="menu-mobile" onClick={handleMenuMobile} />
-        {/* Div ao lado do menu mobile para fechar ao clicar */}
-        <div id="navbar-closeOut" className="navbar-handle-closeOut" onClick={handleMenuMobile}></div>
+        <div
+          id="navbar-closeOut"
+          className="navbar-handle-closeOut"
+          onClick={handleMenuMobile}
+        ></div>
         <ul id="navbar-mobile" className="navbar-menu">
           <AiOutlineClose
             className="menuButtonClose"
             onClick={handleMenuMobile}
           />
-          <li className="nav_button" data-scroll="#home" onClick={scrollToHelper}>HOME</li>
-          <li className="nav_button" data-scroll="#sobre_nos" onClick={scrollToHelper}>SOBRE NÓS</li>
-          <li className="nav_button" data-scroll="#codegenius" onClick={scrollToHelper}>CODEGENIUS</li>
-          <li className="nav_button" data-scroll="#equipe" onClick={scrollToHelper}>EQUIPE</li>
-          <li onClick={onLoginModal}>
-            <FiLogIn />
+          <li
+            className="nav_button"
+            data-scroll="#home"
+            onClick={scrollToHelper}
+          >
+            HOME
+          </li>
+          <li
+            className="nav_button"
+            data-scroll="#sobre_nos"
+            onClick={scrollToHelper}
+          >
+            SOBRE NÓS
+          </li>
+          <li
+            className="nav_button"
+            data-scroll="#codegenius"
+            onClick={scrollToHelper}
+          >
+            CODEGENIUS
+          </li>
+          <li
+            className="nav_button"
+            data-scroll="#equipe"
+            onClick={scrollToHelper}
+          >
+            EQUIPE
+          </li>
+          <li onClick={onLoginModal} className="no_padding">
+            <span class="material-symbols-outlined">login</span>
           </li>
         </ul>
       </div>
