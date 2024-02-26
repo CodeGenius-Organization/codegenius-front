79db4f7173685c2559e75a2014173a24bfabb115
Paula <paula.pires@sptech.school>
2024-02-26 17:25:06 -0300

    Validações de campos

--- src/pages/student/settings/components/user-data/UserData.jsx
+++ src/pages/student/settings/components/user-data/UserData.jsx
@@ -3,6 +3,7 @@ import "./UserData.css";
 import { toast } from "react-toastify";
 import api from "../../../../../Api"
 
+
 function UserData({ data, token }) {
 
   const [teste, setTeste] = useState(data)
@@ -14,8 +15,56 @@ function UserData({ data, token }) {
   const [birthday, setBirthday] = useState()
   const [cep, setCEP] = useState()
 
-  const inputEmail = document.querySelector("#email")
   const inputName = document.querySelector("#name")
+  const inputSurName = document.querySelector("#surname")
+  const inputDataNasc = document.querySelector("#birthday")
+  const inputPhone = document.querySelector("#phone")
+  const inputCep = document.querySelector("#cep")
+
+  function validacaoCampos(){
+
+    if(inputName.value.length < 3 || !(/^[A-Za-z]+$/.test(inputName.value))){
+      toast.error("Nome inválido! Por favor, informe um nome válido")
+      return
+    }
+
+    if(inputSurName.value.length < 3 || !(/^[A-Za-z]+$/.test(inputSurName.value))){
+      toast.error("Sobrenome inválido! Por favor, informe um sobrenome válido")
+      return
+    }
+
+    if(isNaN(inputPhone.value)){
+      toast.error("Telefone inválido! Por favor,insira somente números!.")
+      return
+    }
+    
+    if(inputPhone.value.length < 8){
+      toast.error("Telefone inválido! Por favor, informe um telefone válido")
+      return
+    }
+
+    if(inputDataNasc.value.length < 8){
+      toast.error("Data de nascimento inválida! Por favor, informe uma data válida")
+      return
+    }
+    
+    if(isNaN(inputDataNasc.value)){
+      toast.error("Data de nascimento inválida! Por favor,insira somente números!.")
+    }
+
+
+    if(inputCep.value.length < 9){
+      toast.error("CEP inválido! Por favor, verifique o formato do CEP e tente novamente.")
+      return
+    }
+    
+    if(isNaN(inputCep.value)){
+      toast.error("CEP inválido! Por favor,insira somente números!.")
+      return
+    }
+
+   
+  }
 
   function onPutData(id, token) {
 
@@ -45,97 +94,120 @@ function UserData({ data, token }) {
         }
       })
       .catch((error) => {
-        // switch (error) {
-        //   case 409:
-        //     toast.error("Esse e-mail já está sendo usado!")
-        //     break;
-        //   case 400:
-        //     console.log(error)
-        //     if (error.response.data === "User with this email already exists.") {
-        //       toast.error("Esse e-mail já está sendo usado!")
-        //       return
-        //     }
+        switch (error) {
+          case 409:
+            toast.error("Esse e-mail já está sendo usado!")
+            break;
+          case 400:
+            console.log(error)
+            if (error.response.data === "User with this email already exists.") {
+              toast.error("Esse e-mail já está sendo usado!")
+              return
+            }
 
-        //     const erro = error.response.data.errors
-        //     console.log(erro)
+            const erro = error.response.data.errors
+            console.log(erro)
 
-        //     for (let i = 0; i < erro.length; i++) {
-        //       if (erro[i].field === "password") {
-        //         toast.error("Senha deve ter números, letras maiúsculas e caracteres especiais!")
-        //       }
+            for (let i = 0; i < erro.length; i++) {
+              // if (erro[i].field === "password") {
+              //   toast.error("Senha deve ter números, letras maiúsculas e caracteres especiais!")
+              // }
 
-        //       if (erro[i].field === "email") {
-        //         toast.error("Precisa ser um e-mail bem formatado!")
-        //         return
-        //       }
+              // if (erro[i].field === "email") {
+              //   toast.error("Precisa ser um e-mail bem formatado!")
+              //   return
+              // }
 
-        //       if (erro[i].field === "name") {
-        //         toast.error("Nome inválido, Digite apenas o primeiro nome e um sobrenome!")
-        //       }
-        //     }
-        //     break;
+              if (erro[i].field === "name") {
+                toast.error("Nome inválido, Digite apenas o primeiro nome e um sobrenome!")
+                return
+              }
 
-        //   default:
-        //     break;
-        // }
+              if (error.field === "cep") {
+                toast.error("CEP inválido! Por favor, verifique o formato do CEP e tente novamente.")
+                return
+              }
+            }
+            break;
+
+          default:
+            break;
+        }
+
         console.log(error)
+
+        
       });
 
   }
 
   return (
     <>
+     <script type="text/javascript" src="js/script.js"></script>
       <div className='container-data'>
         <div className='content-data'>
           <span className='title'>Informações Pessoais</span>
           <div className='content-horizontal'>
             <div className='content-txt'>
-              <span>E-mail:</span>
               <span>Primeiro Nome:</span>
-              <span>Último Nome:</span>
+              <span>Sobrenome:</span>
               <span>Telefone:</span>
               <span>Data de Nascimento:</span>
               <span>CEP:</span>
             </div>
             <div className='content-input'>
               <input
-              id="email"
-              onChange={(e) => {
-                setEmail(e.target.value)
-                e.target.classList.remove("error")
-              }} 
-              placeholder={teste.email}></input>
-              <input
               id="name"
+              required
               onChange={(e) => {
                 setName(e.target.value)
                 e.target.classList.remove("error")
               }}
               placeholder={teste.nome}></input>
+
               <input
+              required
+              id='surname'
               onChange={(e) => {
                 setSurname(e.target.value)
                 e.target.classList.remove("error")
               }}
               placeholder='Sobrenome'></input>
+
               <input placeholder='(00) 90000-0000'
+              required
+              id='phone'
+              maxLength= "8"
               onChange={(e) => {
                 setPhone(e.target.value)
                 e.target.classList.remove("error")
               }}></input>
+
               <input placeholder='dd/mm/aaaa'
+              required
+              id='birthday'
+              maxLength= '8'
               onChange={(e) => {
                 setBirthday(e.target.value)
                 e.target.classList.remove("error")
               }}></input>
-              <input placeholder='00000-000'
-              onChange={(e) => {
+
+              <input
+              id="cep"
+              placeholder='00000-000'
+              maxLength = '9'
+              required
+                onChange={(e) => {
                 setCEP(e.target.value)
                 e.target.classList.remove("error")
               }}></input>
+
+
             </div>
           </div>
-          <button onClick={() => onPutData(data.id, token)} >Salvar</button>
+          <button onClick={() => {
+            // onPutData(data.id, token)
+          validacaoCampos() }} >Salvar</button>
         </div>
       </div>
     </>
